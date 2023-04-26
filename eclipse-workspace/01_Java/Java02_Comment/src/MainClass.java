55d19a059e722f437371fd615d91ebbc8d3031f8
Yerin202044071 <yerenkim@gmail.com>
2023-04-26 16:22:12 +0900

    day2

---
+++ eclipse-workspace/01_Java/Java02_Comment/src/MainClass.java
@@ -0,0 +1,18 @@
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// 한 줄 주석
+		// System.out.println("11111");
+		// System.out.println("22222");
+		
+		/* 여러 줄 주석
+		System.out.println("33333");
+		System.out.println("44444");
+		*/
+		
+		System.out.println("55555");
+		System.out.println("66666");
+	}
+
+}
