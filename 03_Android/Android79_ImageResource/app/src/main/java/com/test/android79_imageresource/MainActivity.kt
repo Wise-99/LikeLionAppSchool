9adbfc1a82f0ddff4de0a2b5af7c77191d577a19
YerinKim <yerenkim@gmail.com>
2023-07-13 17:50:02 +0900

    0713 ImageResource

--- 03_Android/Android79_ImageResource/app/src/main/java/com/test/android79_imageresource/MainActivity.kt
+++ 03_Android/Android79_ImageResource/app/src/main/java/com/test/android79_imageresource/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.test.android79_imageresource
 
+import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.test.android79_imageresource.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var activityMainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+
+        activityMainBinding.run{
+            buttonStartAni.setOnClickListener {
+                // 애니메이션 이미지 객체를 추출한다.
+                val animationDrawable = imageViewAni.drawable as AnimationDrawable
+
+                if(animationDrawable.isRunning == true){
+                    // 애니메이션을 중단한다.
+                    animationDrawable.stop()
+                } else {
+                    // 애니메이션을 가동한다.
+                    animationDrawable.start()
+                }
+            }
+        }
+
+        setContentView(activityMainBinding.root)
     }
 }
