615ebab81fde64d1e7ba3c53f79507b7941d8f3b
YerinKim <yerenkim@gmail.com>
2023-07-17 13:11:45 +0900

    0717 Sensor

---
+++ 03_Android/Android85_SensorList/app/src/main/java/com/test/android85_sensorlist/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.test.android85_sensorlist
+
+import android.hardware.Sensor
+import android.hardware.SensorManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.test.android85_sensorlist.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var activityMainBinding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+
+        // 센서 관리자를 가져온다.
+        val sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+
+        // 단말기에 있는 센서 목록을 가져온다.
+        val sensorList = sensorManager.getSensorList(Sensor.TYPE_ALL)
+
+        activityMainBinding.textView.run {
+            text = ""
+
+            // 센서의 수 만큼 반복한다.
+            for(sensor in sensorList){
+                append("센서 이름 : ${sensor.name}\n")
+                append("센서 종류 : ${sensor.type}\n\n")
+            }
+        }
+
+
+
+        setContentView(activityMainBinding.root)
+    }
+}
