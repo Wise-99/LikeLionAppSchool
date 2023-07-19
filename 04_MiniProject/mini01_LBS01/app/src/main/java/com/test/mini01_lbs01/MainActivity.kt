87ec1815b600e755e463b4b51797eb72c2eb3410
YerinKim <yerenkim@gmail.com>
2023-07-19 15:47:45 +0900

    0719 Add 현재 위치 받아 표시, 확대/축소 버튼 추가

--- 04_MiniProject/mini01_LBS01/app/src/main/java/com/test/mini01_lbs01/MainActivity.kt
+++ 04_MiniProject/mini01_LBS01/app/src/main/java/com/test/mini01_lbs01/MainActivity.kt
@@ -1,43 +1,31 @@
 package com.test.mini01_lbs01
 
 import android.Manifest
-import android.annotation.SuppressLint
 import android.content.pm.PackageManager
-import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.SystemClock
-import android.widget.Toast
 import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentTransaction
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.test.mini01_lbs01.PermissionUtils.PermissionDeniedDialog.Companion.newInstance
-import com.test.mini01_lbs01.PermissionUtils.isPermissionGranted
+import com.google.android.gms.maps.UiSettings
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.test.mini01_lbs01.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity(),GoogleMap.OnMyLocationButtonClickListener,
-    GoogleMap.OnMyLocationClickListener, OnMapReadyCallback,
-    ActivityCompat.OnRequestPermissionsResultCallback {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     lateinit var activityMainBinding: ActivityMainBinding
-
-    companion object{
-        private const val LOCATION_PERMISSION_REQUEST_CODE = 1
-    }
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     val permissionList = arrayOf(
         Manifest.permission.ACCESS_FINE_LOCATION,
         Manifest.permission.ACCESS_COARSE_LOCATION
     )
-
-    private var permissionDenied = false
-    private lateinit var map: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
@@ -49,19 +37,21 @@ class MainActivity : AppCompatActivity(),GoogleMap.OnMyLocationButtonClickListen
 
         requestPermissions(permissionList, 0)
 
-        val mapFragment =
-            supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
         mapFragment?.getMapAsync(this)
 
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
         activityMainBinding.run {
+
             toolbar.run {
                 inflateMenu(R.menu.menu)
                 setOnMenuItemClickListener {
                     when(it?.itemId){
-                        R.id.itemChoicePalce -> {
+                        R.id.itemCurrentLocation -> {
 
                         }
-                        R.id.itemCurrentLocation -> {
+                        R.id.itemChoicePlace -> {
 
                         }
                     }
@@ -72,8 +62,8 @@ class MainActivity : AppCompatActivity(),GoogleMap.OnMyLocationButtonClickListen
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
-        map = googleMap
-        enableMyLocation()
+
+        googleMap.uiSettings.isZoomControlsEnabled = true
 
         if (ActivityCompat.checkSelfPermission(
                 this,
@@ -85,112 +75,20 @@ class MainActivity : AppCompatActivity(),GoogleMap.OnMyLocationButtonClickListen
         ) {
             return
         }
-        map.isMyLocationEnabled = true
-        map.setOnMyLocationButtonClickListener(this)
-        map.setOnMyLocationClickListener(this)
-    }
 
-    @SuppressLint("MissingPermission")
-    private fun enableMyLocation() {
+        fusedLocationClient.lastLocation.addOnSuccessListener {
+            if (it != null) {
 
-        // 1. Check if permissions are granted, if so, enable the my location layer
-        if (ContextCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) == PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) == PackageManager.PERMISSION_GRANTED
-        ) {
-            map.isMyLocationEnabled = true
-            return
+                val current = LatLng(it.latitude, it.longitude)
+
+                googleMap.addMarker(
+                    MarkerOptions()
+                        .position(current)
+                        .title("현재 위치")
+                )
+
+                googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(current, 15.0F))
+            }
         }
-
-        // 2. If if a permission rationale dialog should be shown
-        if (ActivityCompat.shouldShowRequestPermissionRationale(
-                this,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) || ActivityCompat.shouldShowRequestPermissionRationale(
-                this,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            )
-        ) {
-            PermissionUtils.RationaleDialog.newInstance(
-                LOCATION_PERMISSION_REQUEST_CODE, true
-            ).show(supportFragmentManager, "dialog")
-            return
-        }
-
-        // 3. Otherwise, request permission
-        ActivityCompat.requestPermissions(
-            this,
-            arrayOf(
-                Manifest.permission.ACCESS_FINE_LOCATION,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ),
-            LOCATION_PERMISSION_REQUEST_CODE
-        )
-    }
-
-    override fun onMyLocationButtonClick(): Boolean {
-        Toast.makeText(this, "내 위치로 이동합니다.", Toast.LENGTH_SHORT)
-            .show()
-        // Return false so that we don't consume the event and the default behavior still occurs
-        // (the camera animates to the user's current position).
-        return false
-    }
-
-    override fun onMyLocationClick(location: Location) {
-        Toast.makeText(this, "현재 위치 :\n$location", Toast.LENGTH_LONG)
-            .show()
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<String>,
-        grantResults: IntArray
-    ) {
-        if (requestCode != LOCATION_PERMISSION_REQUEST_CODE) {
-            super.onRequestPermissionsResult(
-                requestCode,
-                permissions,
-                grantResults
-            )
-            return
-        }
-
-        if (isPermissionGranted(
-                permissions,
-                grantResults,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) || isPermissionGranted(
-                permissions,
-                grantResults,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            )
-        ) {
-            // Enable the my location layer if the permission has been granted.
-            enableMyLocation()
-        } else {
-            // Permission was denied. Display an error message
-            // Display the missing permission error dialog when the fragments resume.
-            permissionDenied = true
-        }
-    }
-
-    override fun onResumeFragments() {
-        super.onResumeFragments()
-        if (permissionDenied) {
-            // Permission was not granted, display error dialog.
-            showMissingPermissionError()
-            permissionDenied = false
-        }
-    }
-
-    /**
-     * Displays a dialog with error message explaining that the location permission is missing.
-     */
-    private fun showMissingPermissionError() {
-        newInstance(true).show(supportFragmentManager, "dialog")
     }
 }
