f063c90ec55c8cadbb4743012ef07f00d84f5b2f
YerinKim <yerenkim@gmail.com>
2023-07-19 14:29:44 +0900

    0719 Splash, GoogleMap 적용

--- 04_MiniProject/mini01_LBS02/app/src/main/java/com/test/mini01_lbs02/MainActivity.kt
+++ 04_MiniProject/mini01_LBS02/app/src/main/java/com/test/mini01_lbs02/MainActivity.kt
@@ -2,10 +2,25 @@ package com.test.mini01_lbs02
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.google.android.gms.maps.MapsInitializer
+import com.test.mini01_lbs02.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var activityMainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // Splash Screen
+        installSplashScreen()
+
+        // 구글 지도 세팅
+        MapsInitializer.initialize(this, MapsInitializer.Renderer.LATEST, null)
+
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
     }
 }
