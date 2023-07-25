6eace6f28d060017d7c277fca6f07390c8b91aff
YerinKim <yerenkim@gmail.com>
2023-07-25 19:49:41 +0900

    0725 Add Login, Join UI 및 화면 간 이동

---
+++ 04_MiniProject/mini02_BoardProject02/app/src/main/java/com/test/mini02_boardproject02/LoginFragment.kt
@@ -0,0 +1,41 @@
+package com.test.mini02_boardproject02
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.test.mini02_boardproject02.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+
+    lateinit var fragmentLoginBinding: FragmentLoginBinding
+    lateinit var mainActivity: MainActivity
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        fragmentLoginBinding = FragmentLoginBinding.inflate(inflater)
+        mainActivity = activity as MainActivity
+
+        fragmentLoginBinding.run{
+            // toolbar
+            toolbarLogin.run{
+                title = "로그인"
+            }
+
+            // 회원가입 버튼
+            buttonLoginJoin.run{
+                setOnClickListener {
+                    // JoinFragment를 보이게 한다.
+                    mainActivity.replaceFragment(MainActivity.JOIN_FRAGMENT, true, null)
+                }
+            }
+        }
+
+        return fragmentLoginBinding.root
+    }
+
+}
--- 04_MiniProject/mini02_BoardProject02/app/src/main/java/com/test/mini02_boardproject02/MainActivity.kt
+++ 04_MiniProject/mini02_BoardProject02/app/src/main/java/com/test/mini02_boardproject02/MainActivity.kt
@@ -10,15 +10,94 @@ import android.view.View
 import android.view.animation.AnticipateInterpolator
 import androidx.core.splashscreen.SplashScreen
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.google.android.material.transition.MaterialSharedAxis
+import com.test.mini02_boardproject02.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var activityMainBinding: ActivityMainBinding
+
+    var newFragment : Fragment? = null
+    var oldFragment: Fragment? = null
+
+    companion object{
+        val LOGIN_FRAGMENT = "LoginFragment"
+        val JOIN_FRAGMENT = "JoinFragment"
+        val ADD_USER_INFO_FRAGMENT = "AddUserInfoFragment"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         val splashScreen = installSplashScreen()
         splashScreenCustomizing(splashScreen)
 
-        setContentView(R.layout.activity_main)
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+
+        replaceFragment(LOGIN_FRAGMENT, false, null)
+    }
+
+    // 지정한 Fragment를 보여주는 메서드
+    fun replaceFragment(name:String, addToBackStack:Boolean, bundle:Bundle?){
+        // Fragment 교체 상태로 설정한다.
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+
+        // newFragment에 Fragment가 들어있으면 oldFragment에 넣어준다.
+        if (newFragment != null){
+            oldFragment = newFragment
+        }
+
+        // 새로운 Fragment를 담을 변수
+        var newFragment = when(name){
+            LOGIN_FRAGMENT -> LoginFragment()
+            JOIN_FRAGMENT -> JoinFragment()
+            ADD_USER_INFO_FRAGMENT -> AddUserInfoFragment()
+            else -> Fragment()
+        }
+
+        newFragment?.arguments = bundle
+
+        if(newFragment != null) {
+
+            // oldFragment -> newFragment로 이동
+            // oldFramgent : exit
+            // newFragment : enter
+
+            // oldFragment <- newFragment 로 되돌아가기
+            // oldFragment : reenter
+            // newFragment : return
+
+            // 애니메이션 설정
+            if (oldFragment != null){
+                oldFragment?.exitTransition = MaterialSharedAxis(MaterialSharedAxis.X, true)
+                oldFragment?.reenterTransition = MaterialSharedAxis(MaterialSharedAxis.X, false)
+                oldFragment?.enterTransition = null
+                oldFragment?.returnTransition = null
+            }
+
+            newFragment?.exitTransition = null
+            newFragment?.reenterTransition = null
+            newFragment?.enterTransition = MaterialSharedAxis(MaterialSharedAxis.X, true)
+            newFragment?.returnTransition = MaterialSharedAxis(MaterialSharedAxis.X, false)
+
+            // Fragment를 교체한다.
+            fragmentTransaction.replace(R.id.mainContainer, newFragment)
+
+            if (addToBackStack == true) {
+                // Fragment를 Backstack에 넣어 이전으로 돌아가는 기능이 동작할 수 있도록 한다.
+                fragmentTransaction.addToBackStack(name)
+            }
+
+            // 교체 명령이 동작하도록 한다.
+            fragmentTransaction.commit()
+        }
+    }
+
+    // Fragment를 BackStack에서 제거한다.
+    fun removeFragment(name:String){
+        supportFragmentManager.popBackStack(name, FragmentManager.POP_BACK_STACK_INCLUSIVE)
     }
 
     // SplashScreen 커스터마이징
