ea5e5ec578555ab154b090e62eb31cfcd6874c51
YerinKim <yerenkim@gmail.com>
2023-07-25 10:45:26 +0900

    0725 Add Splash 화면

--- 04_MiniProject/mini02_BoardProject01/app/src/main/java/com/test/mini02_boardproject01/MainActivity.kt
+++ 04_MiniProject/mini02_BoardProject01/app/src/main/java/com/test/mini02_boardproject01/MainActivity.kt
@@ -1,11 +1,10 @@
 package com.test.mini02_boardproject01
 
-import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
-class MainActivity : Activity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
 
