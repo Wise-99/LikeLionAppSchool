d057618452d11b4c3c258ab9523d4f9de66430fe
YerinKim <yerenkim@gmail.com>
2023-07-27 15:25:44 +0900

    0727 Add 게시글 필터 및 검색 기능

---
+++ 04_MiniProject/mini02_BoardProject02/app/src/main/java/com/test/mini02_boardproject02/PostListFragment.kt
@@ -0,0 +1,106 @@
+package com.test.mini02_boardproject02
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.test.mini02_boardproject02.databinding.FragmentPostListBinding
+import com.test.mini02_boardproject02.databinding.RowPostListBinding
+
+class PostListFragment : Fragment() {
+
+    lateinit var fragmentPostListBinding: FragmentPostListBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        fragmentPostListBinding = FragmentPostListBinding.inflate(inflater)
+
+        fragmentPostListBinding.run {
+            recyclerViewPostAll.run {
+                adapter = AllRecyclerViewAdapter()
+                layoutManager = LinearLayoutManager(context)
+            }
+
+            recyclerViewPostListResult.run {
+                adapter = ResultRecyclerViewAdapter()
+                layoutManager = LinearLayoutManager(context)
+            }
+        }
+
+        return fragmentPostListBinding.root
+    }
+
+    // 모든 게시글 목록을 보여주는 리사이클러 뷰의 어뎁터
+    inner class AllRecyclerViewAdapter : RecyclerView.Adapter<AllRecyclerViewAdapter.AllViewHolder>(){
+        inner class AllViewHolder(rowPostListBinding: RowPostListBinding) : RecyclerView.ViewHolder(rowPostListBinding.root){
+            val rowPostListSubject : TextView
+            val rowPostListNickName:TextView
+
+            init {
+                rowPostListSubject = rowPostListBinding.rowPostListSubject
+                rowPostListNickName = rowPostListBinding.rowPostListNickName
+            }
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AllViewHolder {
+            val rowPostListBinding = RowPostListBinding.inflate(layoutInflater)
+            val allViewHolder = AllViewHolder(rowPostListBinding)
+
+            rowPostListBinding.root.layoutParams = ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+            )
+
+            return allViewHolder
+        }
+
+        override fun getItemCount(): Int {
+            return 100
+        }
+
+        override fun onBindViewHolder(holder: AllViewHolder, position: Int) {
+            holder.rowPostListSubject.text = "제목입니다 : $position"
+            holder.rowPostListNickName.text = "작성자 : $position"
+        }
+    }
+
+    // 모든 게시글 목록을 보여주는 리사이클러 뷰의 어뎁터
+    inner class ResultRecyclerViewAdapter : RecyclerView.Adapter<ResultRecyclerViewAdapter.ResultViewHolder>(){
+        inner class ResultViewHolder(rowPostListBinding: RowPostListBinding) : RecyclerView.ViewHolder(rowPostListBinding.root){
+            val rowPostListSubject : TextView
+            val rowPostListNickName:TextView
+
+            init {
+                rowPostListSubject = rowPostListBinding.rowPostListSubject
+                rowPostListNickName = rowPostListBinding.rowPostListNickName
+            }
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ResultViewHolder {
+            val rowPostListBinding = RowPostListBinding.inflate(layoutInflater)
+            val allViewHolder = ResultViewHolder(rowPostListBinding)
+
+            rowPostListBinding.root.layoutParams = ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+            )
+
+            return allViewHolder
+        }
+
+        override fun getItemCount(): Int {
+            return 100
+        }
+
+        override fun onBindViewHolder(holder: ResultViewHolder, position: Int) {
+            holder.rowPostListSubject.text = "제목입니다 : $position"
+            holder.rowPostListNickName.text = "작성자 : $position"
+        }
+    }
+}
